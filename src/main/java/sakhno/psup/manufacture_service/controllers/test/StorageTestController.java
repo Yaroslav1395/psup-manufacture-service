befe0cc0141cd262d9731629edec12fe9332cb28
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-07-08 09:25:59 +0600

    настройка трассировки запросов

--- src/main/java/sakhno/psup/manufacture_service/controllers/test/StorageTestController.java
+++ src/main/java/sakhno/psup/manufacture_service/controllers/test/StorageTestController.java
@@ -1,4 +1,4 @@
-package sakhno.psup.manufacture_service.controllers;
+package sakhno.psup.manufacture_service.controllers.test;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,73 +9,49 @@ import reactor.core.publisher.Mono;
 import sakhno.psup.manufacture_service.services.web_client.web_client_test.StorageServiceTestClient;
 
 @RestController
-@RequestMapping("api/v1/manufacture-service/test")
+@RequestMapping("api/v1/manufacture-service/test/storage")
 @RequiredArgsConstructor
-public class TestController {
+public class StorageTestController {
     private final StorageServiceTestClient storageServiceTestClient;
-    private Integer count = 0;
 
-    @GetMapping
-    public ResponseEntity<String> test() {
-        count++;
-        System.out.println("Запрос " + count);
-        throw new RuntimeException("Simulated failure");
-    }
-
-    @GetMapping("/timeout")
-    public ResponseEntity<String> timeout() {
-        try {
-            Thread.sleep(5000); // Спим 5 секунд
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return ResponseEntity.status(500).body("Interrupted");
-        }
-        return ResponseEntity.ok("Completed without timeout");
-    }
-
-    @GetMapping("/ok")
-    public ResponseEntity<String> ok() {
-        return ResponseEntity.ok("OK");
-    }
-
-    @GetMapping("/storage/success")
+    @GetMapping("/success")
     public ResponseEntity<Mono<String>> storageSuccess() {
         System.out.println("success: " + Thread.currentThread().getName());
         return ResponseEntity.ok(storageServiceTestClient.successTestRequest());
     }
 
-    @GetMapping("/storage/timeout")
+    @GetMapping("/timeout")
     public ResponseEntity<Mono<String>> storageTimeout() {
         System.out.println("timeout: " + Thread.currentThread().getName());
         return ResponseEntity.ok(storageServiceTestClient.timeoutTestRequest());
     }
 
-    @GetMapping("/storage/error")
+    @GetMapping("/error")
     public ResponseEntity<Mono<String>> storageError() {
         return ResponseEntity.ok(storageServiceTestClient.errorTestRequest());
     }
 
-    @GetMapping("/storage/bad-request")
+    @GetMapping("/bad-request")
     public ResponseEntity<Mono<String>> storageBadRequest() {
         return ResponseEntity.ok(storageServiceTestClient.badRequestTestRequest());
     }
 
-    @GetMapping("/storage/unprocessable-entity")
+    @GetMapping("/unprocessable-entity")
     public ResponseEntity<Mono<String>> storageUnprocessableEntity() {
         return ResponseEntity.ok(storageServiceTestClient.unprocessableEntityTestRequest());
     }
 
-    @GetMapping("/storage/forbidden")
+    @GetMapping("/forbidden")
     public ResponseEntity<Mono<String>> storageForbidden() {
         return ResponseEntity.ok(storageServiceTestClient.forbiddenTestRequest());
     }
 
-    @GetMapping("/storage/not-found")
+    @GetMapping("/not-found")
     public ResponseEntity<Mono<String>> storageNotFound() {
         return ResponseEntity.ok(storageServiceTestClient.notFoundTestRequest());
     }
 
-    @GetMapping("/storage/unauthorized")
+    @GetMapping("/unauthorized")
     public ResponseEntity<Mono<String>> storageUnauthorized() {
         return ResponseEntity.ok(storageServiceTestClient.unauthorizedTestRequest());
     }
